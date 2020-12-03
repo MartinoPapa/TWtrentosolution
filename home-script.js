03de9fe3839eed70ce445d6d3625e9bd481554b3
MartinoPapa <martino.papa.tn@gmail.com>
2020-12-03 13:03:06 +0100

    home page

--- home-script.js
+++ home-script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       animating = false,
       animTime = 500,
       autoSlideTimeout,
-      autoSlideDelay = 7000,
+      autoSlideDelay = 700000,
       $pagination = $(".slider-pagi");
   
   function createBullets() {
