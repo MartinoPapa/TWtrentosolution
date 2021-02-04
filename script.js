9ee93428e546c98c8d347947d68a2cdfc29ad67a
MartinoPapa <martino.papa.tn@gmail.com>
2021-02-04 10:03:59 +0100

    topTino

---
+++ script.js
@@ -0,0 +1,16 @@
+/*function runOnScroll() {
+    SetTitle();
+ }; 
+
+function SetTitle(){
+    var height = window.innerHeight;
+    var top = document.body.getBoundingClientRect().top;
+    if(top*-1 > height/2){
+        document.getElementById("titoloTelefono").innerHTML = "Chi siamo?";
+    }
+    else{
+        document.getElementById("titoloTelefono").innerHTML = "TW Trento Solution";
+    }
+}
+window.addEventListener("scroll", runOnScroll);
+*/
