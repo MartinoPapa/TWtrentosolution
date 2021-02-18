b87f68630a674b016e63c67907f52cf5d5e77ab3
MartinoPapa <martino.papa.tn@gmail.com>
2021-02-18 11:39:06 +0100

    fine

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-/*function runOnScroll() {
-    SetTitle();
- }; 
+function SendEmail(){
+    (function () {
+        emailjs.init("user_PQLnOPIqy0awCPfm5hZbS");
+    })();
 
-function SetTitle(){
-    var height = window.innerHeight;
-    var top = document.body.getBoundingClientRect().top;
-    if(top*-1 > height/2){
-        document.getElementById("titoloTelefono").innerHTML = "Chi siamo?";
+    var templateParams = {
+        tipo: document.getElementById("servizio").value,
+        nome: document.getElementById("nominativo").value,
+        email: document.getElementById("email").value,
+        telefono: document.getElementById("telefono").value,
+        messaggio: document.getElementById("messaggio").value
+    };
+
+    emailjs.send('service_qnfrq9d', 'template_l5t6mhh', templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+}
+
+function SetColor(){
+    input = document.getElementById("servizio");
+    
+    if(input.value == ""){
+        input.style.color = "#888";
     }
     else{
-        document.getElementById("titoloTelefono").innerHTML = "TW Trento Solution";
+        input.style.color = "#000";
     }
 }
-window.addEventListener("scroll", runOnScroll);
-*/
-
